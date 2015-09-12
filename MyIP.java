199b0f6f95a46eafccb2b51bd8a16cf8061831df
Rajiv Lochan Panda <rajivlochanp@gmail.com>
2015-09-12 12:10:10 +0530

    MyIP.java
    Java program to find the IP address of the current terminal

---
+++ MyIP.java
@@ -0,0 +1,12 @@
+import java.net.InetAddress;
+public class MyIP
+{
+	public static void main(String arg[])throws Exception
+	{
+		System.out.println(InetAddress.getLocalHost());
+	}
+}
+/*
+Output:
+watts13/200.0.0.15
+*/
